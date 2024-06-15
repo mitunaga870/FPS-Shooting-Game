578d266c8b9835d248af35414839846e0b612518
umisoon@Desk <umisoon@desk.com>
2024-06-16 02:46:07 +0900

    [add] カメラを動くように

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    private const float MOUSE_SENSITIVETY = 0.8f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var cam = GetComponent<Camera>();
+        if (Input.GetMouseButton(1))
+        {
+            float moveX = Input.GetAxis("Mouse X") * MOUSE_SENSITIVETY;
+            float moveY = Input.GetAxis("Mouse Y") * MOUSE_SENSITIVETY;
+            cam.transform.localPosition -= new Vector3(moveX, 0, moveY);
+        }
+    }
+}
