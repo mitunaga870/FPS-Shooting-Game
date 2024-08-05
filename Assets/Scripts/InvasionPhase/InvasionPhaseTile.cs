67381287420d99456ae550da0b0e99d8c90da8ac
umisoon@Desk <umisoon@desk.com>
2024-08-05 17:25:43 +0900

    [fix] トラップが道になってたバグを修正

--- Assets/Scripts/InvasionPhase/InvasionPhaseTile.cs
+++ Assets/Scripts/InvasionPhase/InvasionPhaseTile.cs
@@ -1,3 +1,4 @@
+using System;
 using AClass;
 using Enums;
 using lib;
@@ -14,13 +15,16 @@ namespace InvasionPhase
             Column = column;
 
             // タイルのステータスによって処理を変える
-            if (tileType == TileTypes.Nothing)
+            switch (tileType)
             {
-                SetNone();
-            }
-            else
-            {
-                SetRoad(roadAdjust);
+                case TileTypes.Nothing:
+                    SetNone();
+                    break;
+                case TileTypes.Road:
+                    SetRoad(roadAdjust);
+                    break;
+                default:
+                    throw new Exception("未対応のタイルタイプです");
             }
         }
     }
