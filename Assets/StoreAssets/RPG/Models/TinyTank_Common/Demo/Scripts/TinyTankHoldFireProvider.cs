dadea576ad811e40b41a70022feeb5b65a3d3642
Ringoameman <upkuro@gmail.com>
2024-08-25 23:47:13 +0900

    add model

---
+++ Assets/StoreAssets/RPG/Models/TinyTank_Common/Demo/Scripts/TinyTankHoldFireProvider.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Pallab.TinyTank
+{
+    [RequireComponent(typeof(TinyTankHoldFireTimer))]
+    public class TinyTankHoldFireProvider : MonoBehaviour
+    {
+        [SerializeField] TinyTankHoldFireTimer holdFireTimer;
+        [SerializeField] Animator animator;
+
+        void OnEnable() => holdFireTimer.OnFire += OnFire;
+
+        void OnDisable() => holdFireTimer.OnFire -= OnFire;
+
+        void OnFire(int index) => animator.SetTrigger($"Fire{index}");
+    }
+}
---
+++ Assets/StoreAssets/RPG/Models/TinyTank_Common/Demo/Scripts/TinyTankHoldInputApplier.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Pallab.TinyTank
+{
+    [RequireComponent(typeof(TinyTankHoldFireTimer))]
+    public class TinyTankHoldInputApplier : MonoBehaviour
+    {
+        [SerializeField] TinyTankHoldFireTimer holdFireTimer;
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                holdFireTimer.StartHold();
+            }
+
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                holdFireTimer.StopHold();
+            }
+        }
+    }
+}
