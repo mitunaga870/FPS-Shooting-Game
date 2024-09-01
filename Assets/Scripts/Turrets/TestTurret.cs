ce45ebe3aae8a72b7b55e6cbd061c47a775213fa
umisoon@Desk <umisoon@desk.com>
2024-09-02 03:08:03 +0900

    [temp] マージ処理用

---
+++ Assets/Scripts/Turrets/TestTurret.cs
@@ -0,0 +1,17 @@
+using AClass;
+
+namespace Turrets
+{
+    public class TestTurret : ATurret
+    {
+        public override float GetHeight()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Awake()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
