a694e9a334d986d5b92c5e41c6231eb49c29efac
sirofyu <127680896+sirofyu@users.noreply.github.com>
2024-10-12 15:59:15 +0900

    Fan#2
    ファンをFで動くように。パーティクルが消える機能は考えてるなう

---
+++ Assets/Scripts/Ignition_action/TrapFan_IgnitionAction.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;  //DOTweenを使うときはこのusingを入れる
+
+
+public class TrapFan_IgnitionAction : MonoBehaviour
+{   
+    [SerializeField] private float X = 0;
+    [SerializeField] private float Y = 1800;
+    [SerializeField] private float Z = 0;
+    [SerializeField] private string ActionKey = "U";
+    [SerializeField] private float time = 4;
+    public enum SampleEnum
+    {
+        None,
+        A,
+        B,
+        C,
+        D,
+    }
+    [SerializeField] private SampleEnum sampleEnum;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //obj.SetActive(false);   // 無効にする
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey (KeyCode.F)) {
+            //obj.SetActive(true);    // 有効にする
+            //time秒間にX,Y,Z度回転させる
+            //https://kingmo.jp/kumonos/dotween-rotate360loop/
+            transform.DOLocalRotate(new Vector3(X,Y,Z),time,RotateMode.FastBeyond360).SetEase(Ease.OutQuad);
+            //DOVirtual.DelayedCall(time, () => obj.SetActive(false));   // 無効にする
+        }
+    }
+}
