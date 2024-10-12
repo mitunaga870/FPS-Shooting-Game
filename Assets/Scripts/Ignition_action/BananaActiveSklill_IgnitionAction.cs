1cb1a9cf14b70a868ef18c33e0786b3cf2b512ff
sirofyu <127680896+sirofyu@users.noreply.github.com>
2024-10-12 23:59:16 +0900

    踏切＃１
    FIX中…

---
+++ Assets/Scripts/Ignition_action/BananaActiveSklill_IgnitionAction.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;  //DOTweenを使うときはこのusingを入れる
+
+public class BananaActiveSklill_IgnitionAction : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey (KeyCode.C)) {
+            //https://qiita.com/sanpeita/items/3864578e852801c15f80
+            
+            
+        }
+    }
+}
