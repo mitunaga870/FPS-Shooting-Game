d6dfc5bc5c431f22332c246f8551841ba163bd03
sirofyu <127680896+sirofyu@users.noreply.github.com>
2024-11-04 01:42:10 +0900

    ada
    d

--- Assets/Scripts/UI_Anim/UI_HoverBigger.cs
+++ Assets/Scripts/UI_Anim/UI_HoverBigger.cs
@@ -12,29 +12,36 @@ using DG.Tweening;
 public class UI_HoverBigger : MonoBehaviour
 {
     [SerializeField] TMP_Text testText;
+    [SerializeField] float up = 0.6f;
+    [SerializeField] float randUP =0.1f;
+    [SerializeField] float randDwn = 0.7f;
+    //[SerializeField] float BiggerU = 80f;
+    [SerializeField] float BiggerD = 80f;
+    [SerializeField] float BiggerAU = 70f;
+    //[SerializeField] float BiggerAD = 70f;
     [SerializeField] GameObject tmpInfo;
     [SerializeField] private int _seed;
 
     public void Start(){
         UnityEngine.Random.InitState(_seed);
-        float randValue = UnityEngine.Random.Range(0.1f, 0.7f);
+        float randValue = UnityEngine.Random.Range(randUP, randDwn);
         Transform obj = this.GetComponent<Transform>();
         //obj.DOPunchPosition(new Vector3(0, 0.5f, 0), 2f, 5, 1f).SetLoops(-1, LoopType.Yoyo).SetRelative(true);
-        obj.DOMove(new Vector3( 0f, 0.5f + randValue, 0.0f) , 2.0f ).SetLoops(-1, LoopType.Yoyo).SetRelative(true);
+        obj.DOMove(new Vector3( 0f, up + randValue, 0.0f) , 2.0f ).SetLoops(-1, LoopType.Yoyo).SetRelative(true);
 
     }
 
     public void OnMouseEnter()
     {
         Debug.Log("ON");
-        this.transform.localScale = new Vector3(80f, 80f, 80f);
+        this.transform.localScale = new Vector3(BiggerD, BiggerD, BiggerD);
         tmpInfo.SetActive(true);
         
     }
     public void OnMouseExit()
     {
         Debug.Log("Out");
-        this.transform.localScale = new Vector3(70f,70f, 70f);
+        this.transform.localScale = new Vector3(BiggerAU,BiggerAU, BiggerAU);
         tmpInfo.SetActive(false);
     }
     
