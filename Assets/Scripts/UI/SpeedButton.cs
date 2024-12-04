0d59130bbf27d067108ef6655f9a30bc0120b33e
Ringoameman <upkuro@gmail.com>
2024-12-04 14:07:57 +0900

    naosi

--- Assets/Scripts/UI/SpeedButton.cs
+++ Assets/Scripts/UI/SpeedButton.cs
@@ -11,23 +11,15 @@ public class SpeedButton : MonoBehaviour
     private InvasionController invasionController;
     bool flag;
 
-    public Component UIEffect;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
     // Update is called once per frame
     void Update()
     {
         if (invasionController.GameState == GameState.FastPlaying && flag == false) {
-            UIEffect.GetComponent<UIEffect>().enabled = true;
+            GetComponent<UIEffect>().enabled = true;
         }
 
         if (invasionController.GameState != GameState.FastPlaying && flag == false) {
-            UIEffect.GetComponent<UIEffect>().enabled = false;
+            GetComponent<UIEffect>().enabled = false;
         }
     }
 }
